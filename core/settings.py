15b9b94856613d148ef1ac45f0a0b4f5bda7702d
Rohan Singh <singhrohanss007@gmail.com>
2021-05-23 01:29:35 +0530

    c8

--- core/settings.py
+++ core/settings.py
@@ -48,8 +48,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # For managing static files in production
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -146,5 +144,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Production Static files
-
+DEFAULT_FILE_STORAGE = 'core.storage_backends.MediaStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 DATA_UPLOAD_MAX_MEMORY_SIZE = 524288000
